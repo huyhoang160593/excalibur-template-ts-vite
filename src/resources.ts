93885ca36d96d0a59b7a6cc49781aa028f93becf
The 99's Puppycat <huyhoang160593@yahoo.com>
2024-07-07 14:23:28 +0700

    custom for bun only

--- src/resources.ts
+++ src/resources.ts
@@ -1,11 +1,47 @@
-import { ImageSource, Loader } from "excalibur";
+import { ImageSource, type Loadable, Loader } from "excalibur";
 import sword from "./images/sword.png";
 
 export const Resources = {
-  Sword: new ImageSource(sword)
-} as const;
+	images: {
+		Sword: new ImageSource(sword),
+	},
+} as const satisfies ResourcesType;
 
 export const loader = new Loader();
-for (const res of Object.values(Resources)) {
-  loader.addResource(res);
+// load all resources in the Resources object
+smartLoader(loader, Resources);
+
+//#region Helper
+type ResourcesType = {
+	[x: string]: ResourcesType | Loadable<unknown>;
+};
+
+function isResourceLoadable(resource: unknown): resource is Loadable<unknown> {
+	return Boolean(
+		resource &&
+			typeof resource === "object" &&
+			"isLoaded" in resource &&
+			typeof resource.isLoaded === "function",
+	);
 }
+
+export function smartLoader(loader: Loader, resources: ResourcesType) {
+	const remainUnloadResources: Array<ResourcesType> = [resources];
+	while (true) {
+		const latestResourcesObject = remainUnloadResources.pop();
+		const resourceValues = Object.values(latestResourcesObject ?? {});
+		for (const resource of resourceValues) {
+			if (isResourceLoadable(resource)) {
+				loader.addResource(resource);
+			} else {
+				remainUnloadResources.push(resource);
+			}
+		}
+
+		if (remainUnloadResources.length === 0) {
+			break;
+		}
+	}
+}
+
+//#endregion
--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
 const { test, expectLoaded, expectPage } = require("@excaliburjs/testing");
 
-test('Vite template', async () => {
-    await expectLoaded();
+test("Vite template", async () => {
+	await expectLoaded();
 
-    await expectPage('main page', './test/images/actual.png').toBe('./test/images/expected.png');
+	await expectPage("main page", "./test/images/actual.png").toBe(
+		"./test/images/expected.png",
+	);
 });
